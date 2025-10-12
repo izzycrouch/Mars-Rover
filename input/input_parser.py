2626ca39ada0907e282530d0c13ec16a1109b471
izzycrouch <belle.crouch00@gmail.com>
2025-10-12 10:28:27 +0100

    Feat: Introduce InstructionsParser class.

--- input/input_parser.py
+++ input/input_parser.py
@@ -1,12 +1,9 @@
 # from input_layer import Instructions, CompassDirection, Position,PlateauSize
 
-class  InstructionsParser:
-    pass
-
 class PlatueSizeParser:
-    def parse(self, input):
+    def parse(self, input_str):
         
-        digits = [int(character) for character in input if character.isdigit()]
+        digits = [int(character) for character in input_str if character.isdigit()]
         
         if not len(digits) == 2:
             raise ValueError('Max plateau size is 9 x 9!')
@@ -14,19 +11,29 @@ class PlatueSizeParser:
         return tuple(digits)
 
 class RoverParser:
-    def __init__(self, name):
+    def parse(self, input_name):
         # check name can only be string
-        if not isinstance(name, str):
+        if not isinstance(input_name, str):
             raise TypeError('Rover name not correct type!')
-        self.name = name
+        self.name = input_name
 
-    def get_valid_name(self):
         # valid names have to be between 3 and 8 characters long
         if not 3 <= len(self.name) <= 8:
             raise ValueError('Valid Rover names have to be between 3 and 8 characters long!')
-        
         # valid names can only be made up of alphanumerics
         if not self.name.isalnum():
             raise ValueError('Valid Rover can only contain alphanumerics!')
-    
+        
         return self.name
+
+class  InstructionsParser:
+    def parse(self, input_str):
+        # turns to correct case
+        upper_str = input_str.upper()
+        # ignores incorrect letters
+        valid_instructions = ['L', 'R', 'M']
+        valid_str = ''.join([character for character in upper_str if character in valid_instructions])
+        return valid_str
+
+class PositionParser:
+    pass
--- test/test_input_parser.py
+++ test/test_input_parser.py
@@ -1,6 +1,7 @@
 import pytest
 
-from input.input_parser import PlatueSizeParser, RoverParser
+from input.input_parser import PlatueSizeParser, RoverParser, InstructionsParser
+#  PositionParser
 
 class TestPlateauParser:
     def test_plateau_class_with_valid_input(self):
@@ -16,92 +17,44 @@ class TestPlateauParser:
         plateau = PlatueSizeParser()
         assert plateau.parse('PLATEAU5x5') == (5, 5)
         
-
 class TestRoverParser:
     def test_rover_name_is_string(self):
         # Test valid name
-        test_r = RoverParser('Rover1')
-        assert test_r.name == 'Rover1'
+        test_r = RoverParser()
+        assert test_r.parse('Rover1') == 'Rover1'
         # Test invalid name - int
-        error_raised = False
-        try:
-            test_r = RoverParser(123)
-        except TypeError:
-            error_raised = True
-        assert error_raised == True
+        test_r = RoverParser()
+        with pytest.raises(TypeError):
+            test_r.parse(123)
         # Test invalid name - list
-        error_raised = False
-        try:
-            test_r = RoverParser(['Rover 1'])
-        except TypeError:
-            error_raised = True
-        assert error_raised == True
+        test_r = RoverParser()
+        with pytest.raises(TypeError):
+            test_r.parse(['Rover 1'])
         # Test invalid name - dict
-        error_raised = False
-        try:
-            test_r = RoverParser({'Name': 'Rover1'})
-        except TypeError:
-            error_raised = True
-        assert error_raised == True
+        test_r = RoverParser()
+        with pytest.raises(TypeError):
+            test_r.parse({'Name': 'Rover1'})
 
     def test_rover_name_is_valid(self):
         # Test valid name returns name if valid
-        test_r = RoverParser('Rover1')
-        assert test_r.get_valid_name() == 'Rover1'
+        test_r = RoverParser()
+        assert test_r.parse('Rover12') == 'Rover12'
         # Test invalid name raises error - special characters
-        test_r = RoverParser('Rover1!')
+        test_r = RoverParser()
         with pytest.raises(ValueError):
-            test_r.get_valid_name()
+            test_r.parse('Rover1!')
             assert True
         # Test invalid name raises error - length
-        test_r = RoverParser('ad')
+        test_r = RoverParser()
         with pytest.raises(ValueError):
-            test_r.get_valid_name()
+            test_r.parse('ad')
             assert True
 
-#     def test_start_co_ordinates_raises_error_if_invalid(self):
-#         test_r = RoverParser('Rover1', '3 3')
-#         assert test_r.get_valid_start_position() == '3 3 N'
-        
-#         test_r = RoverParser('Rover1', '33N')
-#         assert test_r.get_valid_start_position() == '3 3 N'
-
-#         test_r = RoverParser('Rover1', '3 3 N')
-#         assert test_r.get_valid_start_position() == '3 3 N'
-
-#         test_r = RoverParser('Rover1', 'x3, y3, E')
-#         with pytest.raises(ValueError):
-#             test_r.get_valid_start_position()
-#             assert True
-        
-#         test_r = RoverParser('Rover1', 'x3, y3, E')
-#         with pytest.raises(ValueError):
-#             test_r.get_valid_start_position()
-#             assert True
-        
-#         test_r = RoverParser('Rover1', 'abc')
-#         with pytest.raises(ValueError):
-#             test_r.get_valid_start_position()
-#             assert True
-        
-#         test_r = RoverParser('Rover1', '12c')
-#         with pytest.raises(ValueError):
-#             test_r.get_valid_start_position()
-#             assert True
-        
-#     def test_start_co_ordinates_in_plateau(self):
-#         test_r = RoverParser('Rover1', '1 2 N')
-#         test_p = PlatueSizeParser('5 5')
-#         assert test_r.check_starting_postion_in_plateau(test_p) == True
-
-#         test_r = RoverParser('Rover1', '5 6 N')
-#         test_p = PlatueSizeParser('5 5')
-#         with pytest.raises(ValueError):
-#             test_r.check_starting_postion_in_plateau(test_p)
-#             assert True
-        
-#         test_r = RoverParser('Rover1', '12 6 N')
-#         test_p = PlatueSizeParser('5 5')
-#         with pytest.raises(ValueError):
-#             test_r.check_starting_postion_in_plateau(test_p)
-#             assert True
+class TestInstructionsParser:
+    def test_input_string_returns_upper(self):
+        test_i = InstructionsParser()
+        assert  test_i.parse('lrm') == 'LRM'
+    
+    def test_input_string_ignores_incorrect_characters(self):
+        test_i = InstructionsParser()
+        assert  test_i.parse('lrmTHVDAKODMsrm') == 'LRMMRM'
